fd6751cf5343a3e727867930eb410af1bc759fa1
vchaube1 <vedant.chaubey@gmail.com>
2023-06-29 23:52:44 -0700

    server

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Test");
+const http =require('http');
+//const port 3000;
+const server=http.createServer(function(req,res){
+    res.end("Node Server");
+});
+server.listen(3000,() => {
+    console.log("Server running on port 3000");
+});
