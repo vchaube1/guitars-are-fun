a45ee232fc46b4f08bd8666d1fd86a87a5ee3172
vchaube1 <56059690+vchaube1@users.noreply.github.com>
2023-06-30 00:44:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const http =require('http');
 const server=http.createServer(function(req,res){
     res.end("Node Server");
 });
-server.listen(3000,() => {
-    console.log("Server running on port 3000");
+server.listen(80,() => {
+    console.log("Server running on port 80");
 });
